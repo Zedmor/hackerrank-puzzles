01f64002395639f3a575759c791d401bc983d58d
zedmor <zedmor@gmail.com>
2017-03-05 13:23:37 -0500

    sss

--- leetcode/239.py
+++ leetcode/239.py
@@ -49,33 +49,5 @@ nums = [9, 10, 9, -7, -4, -8, 2, -6]
 k = 5
 print(a.maxSlidingWindow(nums, k))
 
-=======
-Example 1:
-Given s = "hello", return "holle".
 
-Example 2:
-Given s = "leetcode", return "leotcede".
-
-Note:
-The vowels does not include the letter "y".
-
-Subscribe to see which companies asked this question.
-"""
-class Solution(object):
-    def reverseVowels(self, s):
-        """
-        :type s: str
-        :rtype: str
-        """
-        s = list(s)
-        vowels = 'AaEeIiOoUu'
-        vwinx = []
-        for i, char in enumerate(s):
-            if char in vowels:
-                vwinx.append(i)
-        for vpos in range(len(vwinx)//2):
-            s[vwinx[vpos]] , s[vwinx[len(vwinx)-vpos-1]] = s[vwinx[len(vwinx)-vpos-1]], s[vwinx[vpos]]
-        return ''.join(s)
-
-print(Solution().reverseVowels("leetcode"))
 >>>>>>>
